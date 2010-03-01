1ecc20065d812fb818e93823844bf13a0fbd016a
Timothée Boucher <timothee@gmail.com>
2010-02-28 17:13:48 -0800

    Uses regex's for matching and create Event object to activate node. Works on most sites now

--- logmeout.js
+++ logmeout.js
@@ -5,35 +5,29 @@
  */
 
 // Bookmarklet:
-// javascript:var%20logMeOut;if(logMeOut!=undefined){logMeOut.manageFailure();}else{var%20script=document.createElement('script');script.src='http://www.logmeoutthx.com/logmeout.js';document.getElementsByTagName('head')[0].appendChild(script);}
+// javascript:var%20logMeOut;if(logMeOut!=undefined){logMeOut.start();}else{var%20script=document.createElement('script');script.src='http://logmeoutthx.com/logmeout.js';document.getElementsByTagName('head')[0].appendChild(script);}
 
 var logMeOut = {
-  stringsToMatch: ["signoff", "signout", "logout", "logoff", "disconnect", // EN
-                  "déconnexion", "sedéconnecter", // FR
+  stringsToMatch: ["sign\\s*off", "sign\\s*out", "log\\s*out", "log\\s*off", "disconnect", // EN
+                  "déconnexion", "se\\s+déconnecter" // FR
     ],
 
-  inArray: function (needle, haystack) {
-    for (var i=0; i < haystack.length; i++) {
-      if (needle == haystack[i]) {
+  isTextAMatch: function (text) {
+    var regexp;
+    for (var i = 0; i < this.stringsToMatch.length; i++) {
+      regexp = new RegExp("\\b"+this.stringsToMatch[i]+"\\b", "i");
+      if (regexp.test(text)) {
         return true;
       }
     }
     return false;
-  },
-
-  isTextAMatch: function (text) {
-    if (this.inArray(text.toLowerCase().replace(" ", ""), this.stringsToMatch)) {
-      return true;
-    } else {
-      return false;
-    }
   },
   
   matchingElts: [],
 
   start: function () {
     this.traverseElement(document.body);
-    if (this.matchingElts.length == 0) {
+    if (this.matchingElts.length === 0) {
       this.nothingFound();
     } else if (this.matchingElts.length == 1) {
       this.findAction(this.matchingElts[0]);
@@ -58,7 +52,7 @@ var logMeOut = {
   },
 
   processNode: function (node) {
-    if (node.tagName && node.tagName.toLowerCase() == "iframe") {
+    if (/iframe/i.test(node.tagName)) {
       if (node.contentDocument) {
         this.traverseElement(node.contentDocument.body);
       }
@@ -73,25 +67,60 @@ var logMeOut = {
   },
   
   findAction: function (element) {
-    if (element.onclick != null) {
-      try {
-        element.onclick();
-      } catch (e) {
-        console.log("Error with onclick:"+e);
-      }
-    } else if (element.href != null && element.href != "" && element.href != "#") {
-      document.location = element.href;
+    if (/body/i.test(element.tagName)) {
+      return;
     } else {
-      this.findAction(element.parentNode);
+      if (!this.activateNode(element)) {
+        this.findAction(element.parentNode);
+      }
     }
+    // if (element.onclick != null) {
+    //   try {
+    //     element.onclick();
+    //   } catch (e) {
+    //     console.log("Error with onclick:"+e);
+    //   }
+    // } else if (element.href != null && element.href != "" && element.href != "#") {
+    //   document.location = element.href;
+    // } else {
+    //   this.findAction(element.parentNode);
+    // }
+  },
+  
+  activateNode: function (element) {
+    var myEvent;
+    if (document.createEventObject) {
+      myEvent = document.createEventObject();
+      return element.fireEvent('onclick', myEvent);
+    } else {
+      myEvent = document.createEvent("HTMLEvents");
+      myEvent.initEvent('click', true, true);
+      return !element.dispatchEvent(myEvent);
+    }
+    
+    
+    
+    // if (typeof element === "object" && element.onclick != null) {
+    //   try {
+    //     var res = element.onclick();
+    //   } catch (e) {
+    //     console.log("Error with onclick: "+e);
+    //   }
+    // }
+    // if (element.href != null && element.href != "" && element.href != "#") {
+    //   setTimeout(function(){document.location = element.href;}, 500);
+    // }
+    // return false;
   },
   
   nothingFound: function() {
     var elt = document.createElement('div');
     elt.id = "logMeOutMessage";
-    elt.innerHTML = '<strong>LogMeOut</strong><br/>That\'s embarrassing but I didn\'t find anything... Maybe you could show me and I\'ll remember for next time.<br/>Just put your mouse cursor over the element you would click to log out and press Alt+Shift+S on your keyboard.<div onclick="logMeOut.showMe()" style="text-decoration: underline;cursor:pointer">Ok, I\'ll show you!</div> <div onclick="logMeOut.nevermind();" style="text-decoration: underline;cursor:pointer">Nevermind...</div>';
+    elt.innerHTML = '<strong>LogMeOut</strong><br/>That\'s embarrassing but I didn\'t find anything...';
+    // elt.innerHTML = '<strong>LogMeOut</strong><br/>That\'s embarrassing but I didn\'t find anything... Maybe you could show me and I\'ll remember for next time.<br/>Just put your mouse cursor over the element you would click to log out and press Alt+Shift+S on your keyboard.<div onclick="logMeOut.showMe()" style="text-decoration: underline;cursor:pointer">Ok, I\'ll show you!</div> <div onclick="logMeOut.nevermind();" style="text-decoration: underline;cursor:pointer">Nevermind...</div>';
     elt.setAttribute('style', "position: fixed; top: 10px; left: 10px; max-width: 350px; background-color: #eeeeee; color: #3b2221; border: 3px solid #666666; padding: 10px;z-index:99999;");
     document.body.appendChild(elt);
+    setTimeout(function(){elt.style.display = "none";}, 3000);
   },
   
   letMeDecide: function() {
@@ -128,7 +157,7 @@ var logMeOut = {
   
   saveTarget: function (event) {
     // var that = this;
-    if (event.altKey == true && event.shiftKey == true && event.keyCode == 205) {
+    if (event.altKey === true && event.shiftKey === true && event.keyCode == 205) {
       console.log(logMeOut.mouseTarget);
       logMeOut.findAction(logMeOut.mouseTarget);
       // do some JSONP magic to send information to server.
@@ -136,7 +165,6 @@ var logMeOut = {
       // findActionFromDescription({id: #id, tagName: #tagName, etc...}) with a Timeout
       // and shows a message like it was saved...
     }
-    console.log('hola');
     console.log(event);
   },
   
