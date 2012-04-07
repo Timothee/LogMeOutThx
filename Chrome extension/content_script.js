a1c538ae59818fa637d8d09cdc6688584febf400
Timothée Boucher <timothee@gmail.com>
2012-04-07 11:38:20 -0700

    Little clean-up

--- Chrome extension/content_script.js
+++ Chrome extension/content_script.js
@@ -1,10 +1,10 @@
 // Script that will be embedded in all pages
 // - retrieves keyboard shortcut from options
 // - inserts LogMeOut script whenever KB shortcut is called
-// The logout part is handled by the remote script on logmeoutthx.com
+// The logout part is handled by the logmeout.js script
 // (c) 2012 Timothée Boucher timotheeboucher.com
 
-var ctrl, alt, shift, keyIdentifier;
+var ctrl, alt, shift, meta, keyIdentifier;
 
 // Retrieves KB shortcut setting
 chrome.extension.sendRequest({command: "restoreOptions"}, restoreOptions);
@@ -19,19 +19,15 @@ function restoreOptions(response) {
 // Listens to keypress and adds script if it matches setting
 window.addEventListener('keydown', launchLogMeOut, false);
 
-function launchLogMeOut (event) {
+function launchLogMeOut(event) {
   if (event.ctrlKey+"" == ctrl &&
       event.altKey+"" == alt &&
       event.shiftKey+"" == shift &&
       event.metaKey+"" == meta &&
       event.keyIdentifier == keyIdentifier) {
-    var logMeOut;
-    if (logMeOut!=undefined) {
-      logMeOut.start();
-    } else {
-      var script=document.createElement('script');
+      var script = document.createElement('script');
       script.src = chrome.extension.getURL('logmeout.js');
-      document.getElementsByTagName('body')[0].appendChild(script);
+      document.body.appendChild(script);
     } // if
   } // if
 } // launchLogMeOut
